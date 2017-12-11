e7ee05b48d133b8c1803bfd0f389c357bc9fde51
Gerard Casas Saez <casassg@gmail.com>
2017-12-10 18:17:42 -0700

    make sure we use floats

--- pagerank.py
+++ pagerank.py
@@ -3,20 +3,20 @@ from pandas import Series
 
 
 def pagerank(df, maxerr=0.0001, d=0.85, max_iterations=100):
-    n = len(df.count())
+    n = float(len(df.count()))
     keys = df.keys()
 
     # dp = Fraction(1, n)
     dp = 1.0 / n
-    rank = Series(data=[dp for _ in range(0, n)], index=keys, dtype=float)
+    rank = Series(data=[dp for _ in range(0, int(n))], index=keys, dtype=float)
     l = dict((key, len(df[key].dropna())) for key in keys)
     for i in range(0, max_iterations):
         print("Iteration %d..." % i)
         ro = rank.copy()
         for key in keys:
-            val = 0
+            val = 0.0
             for t_id in df.loc[:, key].dropna().keys():
-                val += ro[t_id] / l[key]
+                val += float(ro[t_id]) / float(l[key])
 
             rank[key] = (d * val) + ((1.0 - d) / n)
         err = numpy.sum(numpy.abs(rank - ro))
